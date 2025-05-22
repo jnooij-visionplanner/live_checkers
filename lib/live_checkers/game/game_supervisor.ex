0d3166cee204a20adf53ed0653aee89b4d7bf7e5
Julian Nooij <92307442+jnooij-visionplanner@users.noreply.github.com>
2025-05-22 21:41:55 +0200

    Add game supervisor and LiveView

---
+++ lib/live_checkers/game/game_supervisor.ex
@@ -0,0 +1,29 @@
+defmodule LiveCheckers.Game.GameSupervisor do
+  @moduledoc """
+  Dynamic supervisor responsible for running `CheckersGame` processes.
+  """
+  use DynamicSupervisor
+
+  alias LiveCheckers.Game.CheckersGame
+
+  ## Public API
+
+  @doc "Starts the supervisor"
+  def start_link(init_arg \\ :ok) do
+    DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @doc "Starts a new game under the supervisor"
+  def start_game(game_id, players) do
+    child_spec = {CheckersGame, {game_id, players}}
+    DynamicSupervisor.start_child(__MODULE__, child_spec)
+  end
+
+  ## Callbacks
+
+  @impl true
+  def init(:ok) do
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
+end
+
